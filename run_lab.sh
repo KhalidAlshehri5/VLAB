cf90184cd9df0813d8e1cf4e7c94bd456d3e419e
KhalidAlshehri5 <kz5@tuta.io>
2024-12-10 00:04:16 +0300

    Update run_lab.sh

--- run_lab.sh
+++ run_lab.sh
@@ -1,39 +1,31 @@
-#!/bin/bash
-
 echo "Available Labs:"
 echo "1) Juice Shop"
 echo "2) Nmap"
-echo "3) Metasploit"
+echo "3) metasploit"
 echo "4) Kali Linux"
 echo ""
 
-read -p "Select the lab you want to run (1-4): " lab_choice
-
-case $lab_choice in
-    1)
-        echo "Starting Juice Shop..."
-        sudo docker run -d -p 3000:3000 alucard5/vlab-tools npm start
-        ;;
-    2)
-        echo "Starting Nmap..."
-        # Check if Nmap is installed
-        if ! command -v nmap &> /dev/null
-        then
-            echo "Nmap not found. Installing Nmap..."
-            sudo apt update
-            sudo apt install -y nmap
-        fi
-        sudo docker run -it alucard5/vlab-tools nmap
-        ;;
-    3)
-        echo "Starting Metasploit..."
-        sudo docker run -it alucard5/vlab-tools msfconsole
-        ;;
-    4)
-        echo "Starting Kali Linux..."
-        sudo docker run -it alucard5/vlab-tools /bin/bash
-        ;;
-    *)
-        echo "Invalid selection. Please choose a number between 1 and 4."
-        ;;
+read -p "select the lab you want to run (1-4)" lab_choice
+case $lab_choice in 
+1)
+echo "starting Juice Shop... "
+sudo docker run -d -p 4000:3000 -w /app alucard5/vlab-tools nmp start
+;;
+2)
+echo "starting Nmap... "
+sudo docker run -it alucard5/vlab-tools nmap
+;;
+3)
+echo "starting Metasploit... "
+sudo docker run -it alucard5/vlab-tools msfconsole
+;;
+4)
+echo "starting Kali Linux... "
+sudo docker run -it alucard5/vlab-tools /bin/bash
+;;
+*)
+echo "Invalid selection"
+;;
 esac
+
+
